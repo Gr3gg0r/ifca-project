41ea7eedb2490ebdab0ecd0078447beaa722ea6d
Shahfiq Shah <shahfiq12@gmail.com>
2021-09-04 10:22:01 +0800

    Complete backend

---
+++ src/controllers/auth.js
@@ -0,0 +1,59 @@
+const Joi = require("joi")
+
+const users = [
+    {
+        id:1,
+        email:"shahfiq@mail.com",
+        password : "asasdasd"
+    },
+    {
+        id:2,
+        email:"shahfq12@mail.com",
+        password : "asasdasd"
+    },
+]
+
+function validateBody(data){
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required()
+    })
+
+    return schema.validate(data);
+}
+
+function validatePassword(data) {
+    const user = users.find((e)=>e.email==data.email);
+
+    if (!user) return false;
+    if (user.password === data.password) return true;
+    else return false;
+}
+
+const login = (req,res) => {
+    const { error } = validateBody(req.body);
+    if (error) return res.status(400).send({msg:error.details[0].message});
+
+    const validation = validatePassword(req.body);
+    if (!validation) return res.send("Email or password is incorrect");
+    
+    res.send(true);
+}
+
+const register = (req,res)=>{
+    const { error } = validateBody(req.body);
+    if (error) return res.status(400).send({msg:error.details[0].message});
+
+    const user = users.find((e)=>e.email==req.body.email);
+    if (user) return res.send("Email already use");
+    
+    users.push({
+        id:users.length+1,
+        email:req.body.email,
+        password:req.body.password,
+    })
+
+    res.send(true);
+}
+
+module.exports = {login,register}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const helmet = require('helmet');
 const cors = require('cors');
 const express = require('express');
-
+const auth = require('./routes/authentication');
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
+app.use(express.static(__dirname + '/public/dist/spa'));
+
+app.use('/api/auth',auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port,() => {
